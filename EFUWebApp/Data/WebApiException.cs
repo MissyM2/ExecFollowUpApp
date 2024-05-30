0913af9ee2782950dbc8e0dc43b340380a607d0f
missymaloney <missymaloney1@gmail.com>
2024-05-30 09:52:09 -0400

    Section 5. 41. Error handling in Api Executor

---
+++ EFUWebApp/Data/WebApiException.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using EFUWebApp.Data;
+
+namespace EFUWebApp;
+
+public class WebApiException : Exception
+{
+  public ErrorResponse? ErrorResponse { get;}
+
+  public WebApiException(string errorJson)
+  {
+    ErrorResponse = JsonSerializer.Deserialize<ErrorResponse>(errorJson);
+  }
+
+}
--- EFUWebApp/Data/WebApiExecutor.cs
+++ EFUWebApp/Data/WebApiExecutor.cs
@@ -1,4 +1,7 @@
-﻿namespace EFUWebApp;
+﻿using System.Text.Json;
+using EFUWebApp.Data;
+
+namespace EFUWebApp;
 
 public class WebApiExecutor : IWebApiExecutor
 {
@@ -12,14 +15,27 @@ public class WebApiExecutor : IWebApiExecutor
   public async Task<T?> InvokeGet<T>(string relativeUrl)
   {
     var httpClient = httpClientFactory.CreateClient(apiName);
-    return await httpClient.GetFromJsonAsync<T>(relativeUrl);
+    // for exception handling, we do not have a response to work with
+    // so, we are commenting the next line (return)
+    // and using another method, so we can get a response
+    //return await httpClient.GetFromJsonAsync<T>(relativeUrl);
+    var request = new HttpRequestMessage(HttpMethod.Get, relativeUrl);
+    var response = await httpClient.SendAsync(request);
+
+    // by substituting the above 2 lines, we get a response and now can call our 
+    // exception handling method
+    await HandlePotentialError(response);
+
+    return await response.Content.ReadFromJsonAsync<T>();
+
   }
 
   public async Task<T?> InvokePost<T>(string relativeUrl, T obj)
   {
     var httpClient = httpClientFactory.CreateClient(apiName);
     var response = await httpClient.PostAsJsonAsync(relativeUrl, obj);
-    response.EnsureSuccessStatusCode();
+    
+    await HandlePotentialError(response);
 
     return await response.Content.ReadFromJsonAsync<T>();
   }
@@ -28,14 +44,26 @@ public class WebApiExecutor : IWebApiExecutor
   {
     var httpClient = httpClientFactory.CreateClient(apiName);
     var response = await httpClient.PutAsJsonAsync(relativeUrl, obj);
-    response.EnsureSuccessStatusCode();
+    
+    await HandlePotentialError(response);
   }
 
   public async Task InvokeDelete(string relativeUrl)
   {
     var httpClient = httpClientFactory.CreateClient(apiName);
     var response = await httpClient.DeleteAsync(relativeUrl);
-    response.EnsureSuccessStatusCode();
+    
+    await HandlePotentialError(response);
+  }
+
+  private async Task HandlePotentialError(HttpResponseMessage httpResponse)
+  {
+    if (!httpResponse.IsSuccessStatusCode)
+    {
+      var errorJson = await httpResponse.Content.ReadAsStringAsync();
+      throw new WebApiException(errorJson);
+    }
+
   }
 
 }
