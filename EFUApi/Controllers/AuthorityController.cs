ec0e6ca536215e946f10833817352f9586d61018
missymaloney <missymaloney1@gmail.com>
2024-05-31 17:47:29 -0400

    Section 6. 49: Generate JWT token

--- EFUApi/Controllers/AuthorityController.cs
+++ EFUApi/Controllers/AuthorityController.cs
@@ -1,20 +1,34 @@
-﻿using EFUApi.Authority;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using EFUApi.Authority;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EFUApi.Controllers;
 
 [ApiController]
 public class AuthorityController : ControllerBase
 {
+
+  private readonly IConfiguration configuration;
+
+  public AuthorityController(IConfiguration configuration)
+  {
+    this.configuration = configuration;
+  }
+
   [HttpPost("auth")]
   public IActionResult Authenticate([FromBody]AppCredential credential)
   {
     if (AppRepository.Authenticate(credential.ClientId, credential.Secret))
     {
+      var expiresAt = DateTime.UtcNow.AddMinutes(10);
+
       return Ok(new
       {
-        access_token = CreateToken(credential.ClientId),
-        expires_at = DateTime.UtcNow.AddMinutes(10)
+        access_token = CreateToken(credential.ClientId, expiresAt),
+        expires_at = expiresAt
       });
     }
     else
@@ -28,9 +42,34 @@ public class AuthorityController : ControllerBase
     }
   }
 
-  private string CreateToken(string clientId)
+  private string CreateToken(string clientId, DateTime expiresAt)
   {
-    return string.Empty;
+
+    // There are 3 parts to a token
+    // Algorithm
+    // Payload (claims - application information)
+    // Signing Key
+
+    var app = AppRepository.GetApplicationByClientId(clientId);
+
+    var claims = new List<Claim>
+    {
+      new Claim("AppName", app.ApplicationName??string.Empty),
+      new Claim("Read", (app?.Scopes??string.Empty).Contains("read")?"true":"false"),
+      new Claim("Write", (app?.Scopes??string.Empty).Contains("write")?"true":"false")
+    };
+
+    var secretKey = Encoding.ASCII.GetBytes(configuration.GetValue<string>("SecretKey"));
+
+    var jwt = new JwtSecurityToken(
+      signingCredentials: new SigningCredentials(
+        new SymmetricSecurityKey(secretKey),
+        SecurityAlgorithms.HmacSha256Signature),
+        claims: claims,
+        expires: expiresAt,
+        notBefore: DateTime.UtcNow
+      );
+    return new JwtSecurityTokenHandler().WriteToken(jwt);
   }
 
 }
